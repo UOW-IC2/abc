34d9966984d79829c1595d30eeca121f9e82fa14
nan864 <63390896+nan864@users.noreply.github.com>
2024-08-26 18:05:16 +1000

    Update get_script_time.js

--- get_script_time.js
+++ get_script_time.js
@@ -3,7 +3,7 @@
 const start = performance.now();
 
 const script = document.createElement('script');
-script.src = 'https://www.gstatic.com/feedback/js/help/prod/service/lazy.min.js';
+script.src = 'https://fonts.gstatic.com/s/googlesans/v58/4UasrENHsxJlGDuGo1OIlJfC6l_24rlCK1Yo_Iqcsih3SAyH6cAwhX9RPjIUvbQoi-E.woff2';
 script.onload = () => {
     const end = performance.now();
     console.log(`Script loaded in ${end - start} milliseconds`);
