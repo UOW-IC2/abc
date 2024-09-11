95ab0f7dbffaa54159bcb1e7812057f5ef3e044d
nan864 <63390896+nan864@users.noreply.github.com>
2024-09-11 15:54:40 +1000

    Update get_script_time.js

--- get_script_time.js
+++ get_script_time.js
@@ -1,12 +1,12 @@
+const id = 12345;
 
+// const start = performance.now();
 
-const start = performance.now();
+// const script = document.createElement('script');
+// script.src = 'https://fonts.gstatic.com/s/googlesans/v58/4UasrENHsxJlGDuGo1OIlJfC6l_24rlCK1Yo_Iqcsih3SAyH6cAwhX9RPjIUvbQoi-E.woff2';
+// script.onload = () => {
+//     const end = performance.now();
+//     console.log(`Script loaded in ${end - start} milliseconds`);
+// };
 
-const script = document.createElement('script');
-script.src = 'https://fonts.gstatic.com/s/googlesans/v58/4UasrENHsxJlGDuGo1OIlJfC6l_24rlCK1Yo_Iqcsih3SAyH6cAwhX9RPjIUvbQoi-E.woff2';
-script.onload = () => {
-    const end = performance.now();
-    console.log(`Script loaded in ${end - start} milliseconds`);
-};
-
-document.head.appendChild(script);
+// document.head.appendChild(script);
